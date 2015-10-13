83909a9220cdb90a4f10508672aa022d9c8d1e52
Brian Larson <brla6495@colorado.edu>
2015-10-12 18:31:25 -0600

    Added Tabs and Icons

---
+++ Conversion App/Conversion App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Conversion App
+//
+//  Created by Brian Larson on 10/12/15.
+//  Copyright © 2015 CSCI3308 Project Team. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Conversion App/Conversion App/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Conversion App
+//
+//  Created by Brian Larson on 10/12/15.
+//  Copyright © 2015 CSCI3308 Project Team. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
